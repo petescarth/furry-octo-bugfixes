b044c06725e4a6a4503481fc1dc7a0f64cb4835d
Peter Scarth <peter.scarth@gmail.com>
2014-06-27 10:00:32 +1000

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-sudo yum install -y gcc-c++ python-devel python-pip
-sudo pip install flask
-
-
-git config  --global core.editor "nano"
-git config --global user.name "Peter Scarth"
-git config --global user.email "peter.scarth@gmail.com"
-git config --global color.ui auto
-git remote set-url origin https://petescarth@github.com/petescarth/furry-octo-bugfixes.git  
